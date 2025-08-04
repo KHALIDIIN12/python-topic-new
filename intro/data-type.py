8f41f4a3b53a9f7014d00b7425b1103fbb3603e7
KHALIDIIN12 <khalidinho53@gmail.com>
2025-08-04 15:56:38 +0300

    finished data-types

--- intro/data-type.py
+++ intro/data-type.py
@@ -1,17 +1,27 @@
 #hi
 
 x=10 #Integer 
+print(x)
 pi=3.142  #Floating
-myName="Daniel" #String
-myName1='Daniel' #String
+print(pi)
+myName="khalid" #String
+print(myName)
+myName1='khalidiin' #String
+print(myName)
 is_happy=True # python True/False js true/false
+print(is_happy)
 no_value=None # None is the equivalent to Null in js
+print(no_value)
 
 #Advanced Datatypes Bracket notation
 my_list=[10,10,"string",True,None,123,[]] # List<Array> 
+print(my_list)
 my_set={1,2,3,3,"Hello","Hello"} #Set(unique stuff only) and Array
+print(my_set)
 my_tuple=(1,3,4,5,"Apple") #tuple my_tuple[4]=Apple
+print(my_tuple)
 my_obj={"key":"value","age":23} #Dictionary Key<->value my_obj["key"]
+print(my_obj)
 
 
-
+ 
