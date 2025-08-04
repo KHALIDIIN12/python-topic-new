932a67b02dd12ce8cc680bdf385e8bf579edce9b
KHALIDIIN12 <khalidinho53@gmail.com>
2025-08-04 16:23:30 +0300

    make conditions file and code

---
+++ intro/conditions.py
@@ -0,0 +1,10 @@
+age=21
+
+if age>20:
+    print("you can drink")
+
+if age<20:
+    print("wait lil bro")
+
+if age<12:
+    print("go drink milk")
--- intro/data-type.py
+++ intro/data-type.py
@@ -1,27 +1,23 @@
 #hi
 
 x=10 #Integer 
-print(x)
+print(type(x))
 pi=3.142  #Floating
-print(pi)
+print(type(pi))
 myName="khalid" #String
-print(myName)
-myName1='khalidiin' #String
-print(myName)
+print(type(myName))
 is_happy=True # python True/False js true/false
-print(is_happy)
+print(type(is_happy))
 no_value=None # None is the equivalent to Null in js
-print(no_value)
-
+print(type(no_value))
 #Advanced Datatypes Bracket notation
 my_list=[10,10,"string",True,None,123,[]] # List<Array> 
-print(my_list)
+print(type(my_list))
 my_set={1,2,3,3,"Hello","Hello"} #Set(unique stuff only) and Array
-print(my_set)
+print(type(my_set))
 my_tuple=(1,3,4,5,"Apple") #tuple my_tuple[4]=Apple
-print(my_tuple)
+print(type(my_tuple))
 my_obj={"key":"value","age":23} #Dictionary Key<->value my_obj["key"]
-print(my_obj)
-
+print(type(my_obj))
 
  
