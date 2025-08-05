1b56f3c1217b6cf221cf9b17c81e71087b586198
KHALIDIIN12 <khalidinho53@gmail.com>
2025-08-05 10:20:43 +0300

    added function and args.py

---
+++ intro/args.py
@@ -0,0 +1,5 @@
+def  greet(*args):
+    for value in args:
+        print("Name is",value)
+        
+greet("John","Samuel","Mandy")
---
+++ intro/loop.py
@@ -0,0 +1,37 @@
+o_loop=True
+i=0
+while to_loop:
+   if i>10:
+      to_loop=False
+   print("i is",i)
+   i=i+1 
+
+
+k=0
+while k<10:
+   print("k is",k)
+   k=k+1
+
+
+
+
+for i in range(2,10):
+   print("For loop is is",i)
+
+#from 10 to  2
+for i in range(10,2,-1):
+   print("For loop is is",i)
+
+
+for i in range(0,1000,5):
+   print("All even numbers",i)
+
+fruits=["Mango","Papaya","Orange"]
+
+for fruit in fruits:
+   print(fruit)
+
+for i in range(0,3):
+   
+    fruit=fruits[i]
+    print(fruit)
